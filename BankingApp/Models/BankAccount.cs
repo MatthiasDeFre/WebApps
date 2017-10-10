b58583a48ed91dc265122f267520aea0091ee971
Matthias De Fre <matthias.defre.y7363@student.hogent.be>
2017-10-10 16:06:57 +0200

    Add project files.

---
+++ BankingApp/Models/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingApp.Models
+{
+    class BankAccount
+    {
+        private string _accountnumber;
+        private decimal _balance;
+        public 
+
+        public string AccountNumber { get; set; }
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}
